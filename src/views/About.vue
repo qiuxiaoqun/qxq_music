82f31bfae56b0e506e00375ccbae26ac9ec785d0
DESKTOP-O29A8SE\asus <1710248836@>
2021-03-22 18:18:05 +0800

    add

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    相关
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+      首页
+  </div>
+</template>
+
+<script>
+
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
